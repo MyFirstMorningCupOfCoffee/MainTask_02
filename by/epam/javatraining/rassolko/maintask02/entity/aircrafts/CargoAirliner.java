3cee9b2e5f8a9415cf634a280abee86d684b40ae
Aliaksandr Rasolka <47742350+MyFirstMorningCupOfCoffee@users.noreply.github.com>
2019-04-01 05:42:02 +0300

    Add files via upload

---
+++ by/epam/javatraining/rassolko/maintask02/entity/aircrafts/CargoAirliner.java
@@ -0,0 +1,42 @@
+package by.epam.javatraining.rassolko.maintask02.entity.aircrafts;
+
+public class CargoAirliner extends Plane
+{
+    // These parameters describes size of loading gates of this cargo airliner.
+    // Cargo can require really lot of space
+    private double loadingGateHeightMeters;
+    private double loadingGateWidthMeters;  
+
+    
+    /**
+     * @return the loadingGateHeightMeters
+     */
+    public double getLoadingGateHeightMeters()
+    {
+        return loadingGateHeightMeters;
+    }
+
+    /**
+     * @param loadingGateHeightMeters the loadingGateHeightMeters to set
+     */
+    public void setLoadingGateHeightMeters(double loadingGateHeightMeters)
+    {
+        this.loadingGateHeightMeters = loadingGateHeightMeters;
+    }
+
+    /**
+     * @return the loadingGateWidthMeters
+     */
+    public double getLoadingGateWidthMeters()
+    {
+        return loadingGateWidthMeters;
+    }
+
+    /**
+     * @param loadingGateWidthMeters the loadingGateWidthMeters to set
+     */
+    public void setLoadingGateWidthMeters(double loadingGateWidthMeters)
+    {
+        this.loadingGateWidthMeters = loadingGateWidthMeters;
+    }
+}
---
+++ by/epam/javatraining/rassolko/maintask02/entity/aircrafts/PassengerAirliner.java
@@ -0,0 +1,24 @@
+package by.epam.javatraining.rassolko.maintask02.entity.aircrafts;
+
+public class PassengerAirliner extends Plane
+{
+    // number of passenger seats in current passenger airliner
+    private int seats;
+
+    /**
+     * @return the seats
+     */
+    public int getSeats()
+    {
+        return seats;
+    }
+
+    /**
+     * @param seats the seats to set
+     */
+    public void setSeats(int seats)
+    {
+        this.seats = seats;
+    }
+    
+}
